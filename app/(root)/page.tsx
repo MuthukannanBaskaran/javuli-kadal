dce81c7df1367b1189a2fd05386b49965bf74534
Muthukannan Baskaran <69107436+MuthukannanBaskaran@users.noreply.github.com>
2026-05-13 14:53:36 +0530

    header and footer layout added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+export const metadata = {
+  title:"Javuli Kadal Air"
+}
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>Javuli Kadal Website</h1>
-      <Button>Javuli Kadal Button</Button>
     </div>
   );
 }
